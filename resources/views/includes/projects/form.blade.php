d5fddad92fa04fecbe0d87a551ff15ca21fc835c
antonionovelli92 <anto.novelli@hotmail.it>
2023-03-12 10:48:28 +0100

    script slug

--- resources/views/includes/projects/form.blade.php
+++ resources/views/includes/projects/form.blade.php
@@ -9,7 +9,7 @@
     {{-- Titolo --}}
     <div class="col-md-4">
         <div class="mb-3">
-            <label for="title">Titolo</label>
+            <label for="title" class="form-label">Titolo</label>
             <input type="text" name="title" id="title"
                 class="form-control @error('title') is-invalid @enderror" value="{{ old('title', $project->title) }}"
                 required minlength="3" maxlength="80">
@@ -26,20 +26,15 @@
     {{-- Slug --}}
     <div class="col-md-4">
         <div class="mb-3">
-            <label for="slug">Slug</label>
-            <input type="text" name="slug" id="slug" class="form-control @error('slug') is-invalid @enderror"
-                value="{{ old('slug', $project->slug) }}">
-            @error('title')
-                <div class="invalid-feedback">
-                    {{ $message }}
-                </div>
-            @enderror
+            <label for="slug" class="form-label">Slug</label>
+            <input type="text" id="slug" class="form-control"
+                value="{{ Str::slug(old('title', $project->title), '-') }}" disabled>
         </div>
     </div>
     {{-- Autore --}}
     <div class="col-md-4">
         <div class="mb-3">
-            <label for="author">Autore</label>
+            <label for="author" class="form-label">Autore</label>
             <input type="text" name="author" id="author"
                 class="form-control @error('author') is-invalid @enderror" value="{{ old('author', $project->author) }}"
                 required minlength="3" maxlength="80">
@@ -55,7 +50,7 @@
     {{-- Descrizione --}}
     <div class="col-md-4">
         <div class="mb-3">
-            <label for="description">Descrizione</label>
+            <label for="description" class="form-label">Descrizione</label>
             <input type="text" name="description" id="description"
                 class="form-control @error('description') is-invalid @enderror"
                 value="{{ old('description', $project->description) }}">
@@ -73,7 +68,7 @@
     {{-- Immagine --}}
     <div class="col-md-8">
         <div class="mb-3">
-            <label for="image">Immagine</label>
+            <label for="image" class="form-label">Immagine</label>
             <input type="url" name="image" id="image"
                 class="form-control @error('image') is-invalid @enderror" value="{{ old('image', $project->image) }}">
             @error('title')
@@ -88,7 +83,7 @@
     {{-- Contenuto --}}
     <div class="col-md-12">
         <div class="mb-3">
-            <label for="content">Contenuto</label>
+            <label for="content" class="form-label">Contenuto</label>
             <textarea name="content" id="content" class="form-control @error('content') is-invalid @enderror" rows="10">{{ old('content', $project->content) }}</textarea>
             @error('title')
                 <div class="invalid-feedback">
@@ -103,7 +98,7 @@
     <div class="d-flex justify-content-between align-items-center">
         <div class="col-md-4">
             <div class="mb-3">
-                <label for="url_project">URL Progetto</label>
+                <label for="url_project" class="form-label">URL Progetto</label>
                 <input type="url" name="url_project" id="url_project"
                     class="form-control @error('url_project') is-invalid @enderror"
                     value="{{ old('url_project', $project->url_project) }}">
@@ -120,7 +115,7 @@
 
         <div class="col-md-4">
             <div class="mb-3">
-                <label for="url_generic">URL Generico</label>
+                <label for="url_generic" class="form-label">URL Generico</label>
                 <input type="url" name="url_generic" id="url_generic"
                     class="form-control @error('url_generic') is-invalid @enderror"
                     value="{{ old('url_generic', $project->url_generic) }}">
@@ -147,15 +142,18 @@
         <i class="fa-solid fa-floppy-disk"></i>
     </button>
 </footer>
-
-
-
-
-
-
-
-
-
-
-
 </form>
+
+@section('scripts')
+    <script>
+        //Prendo gli elementi dal dom
+        const slugInput = document.getElementById('slug');
+        const titleInput = document.getElementById('title');
+
+        // Metto un event listner al title (uso ' blur ', quando esco dal focus incomincia l'azione)
+
+        titleInput.addEventListener('blur', () => {
+            slugInput.value = titleInput.value.toLowerCase().split(' ').join('-');
+        })
+    </script>
+@endsection
